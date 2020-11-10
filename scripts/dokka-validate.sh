d1e40e5a8abd5ae390bcf5511631f8ff95f3934e
Yury Kanetski <yury.kanetski@mapbox.com>
2020-11-10 19:23:35 +0300

    dokka updated to 1.4.20

--- scripts/dokka-validate.sh
+++ scripts/dokka-validate.sh
@@ -2,15 +2,16 @@
 
 EXIT_TYPE="exit 1"
 MODULE=""
+GRADLE_TASK="dokkaHtmlMultiModule"
 
-SEARCH_REGEX='No documentation for com.mapbox.navigation'
+SEARCH_REGEX='Undocumented: com.mapbox.navigation.'
 SEARCH_REGEX_EXCEPT='\.Companion ' # skip `Companion object` headers
 
 while getopts 'sm:' c
 do
   case $c in
     s) EXIT_TYPE="" ;; # soft failing (optional)
-    m) MODULE="${OPTARG}:";; # module to validate dokka (optional)
+    m) MODULE="${OPTARG}:" && GRADLE_TASK="${MODULE}:dokkaHtml";; # module to validate dokka (optional)
   esac
 done
 
@@ -18,6 +19,6 @@ done
 echo "Dokka validation is starting; clean project"
 ./gradlew clean
 echo "Dokka is validating ${MODULE}"
-./gradlew "${MODULE}dokka" | grep -i "$SEARCH_REGEX" | grep -v "$SEARCH_REGEX_EXCEPT" && { echo 'kdoc validation failed'; ${EXIT_TYPE}; }
+./gradlew $GRADLE_TASK | grep -i "$SEARCH_REGEX" | grep -v "$SEARCH_REGEX_EXCEPT" && { echo 'kdoc validation failed'; ${EXIT_TYPE}; }
 echo "Dokka validation has finished"
 exit 0
